4922dcaa755f3de439e07d76179b63ad6736d9f0
calebdeji <calebdeji06@gmail.com>
2021-05-02 15:46:50 +0100

    ✨ feature: add colors to use

---
+++ lib/constants/colors.dart
@@ -0,0 +1,14 @@
+import 'package:fakeCall/utils/helpers.dart';
+import 'package:flutter/rendering.dart';
+
+final Color primaryColor = getColorFromHexValue('3580FF');
+final Color gray100 = getColorFromHexValue("828282");
+final Color black100 = getColorFromHexValue("000000");
+final Color black200 = getColorFromHexValue("001029");
+final Color black300 = getColorFromHexValue("3E3E3E");
+final Color red100 = getColorFromHexValue("EB5757");
+final Color blue200 = getColorFromHexValue("2F80ED");
+final Color white100 = getColorFromHexValue("FFFFFF");
+final Color white200 = getColorFromHexValue("E0E0E0");
+final Color yellow100 = getColorFromHexValue("FFCB27");
+final Color yellow200 = getColorFromHexValue("FFE361");
