d061df486211b4c9b928208c81ba296ce3bb849f
calebdeji <calebdeji06@gmail.com>
2021-05-04 10:40:06 +0100

    ✨ feature: add Home UI and implementation

---
+++ lib/ui/views/Home/Home.form.dart
@@ -0,0 +1,41 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// StackedFormGenerator
+// **************************************************************************
+
+// ignore_for_file: public_member_api_docs
+
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+
+const String PhoneNumberValueKey = 'phoneNumber';
+
+mixin $HomeView on StatelessWidget {
+  final TextEditingController phoneNumberController = TextEditingController();
+  final FocusNode phoneNumberFocusNode = FocusNode();
+
+  /// Registers a listener on every generated controller that calls [model.setData()]
+  /// with the latest textController values
+  void listenToFormUpdated(FormViewModel model) {
+    phoneNumberController.addListener(() => _updateFormData(model));
+  }
+
+  /// Updates the formData on the FormViewModel
+  void _updateFormData(FormViewModel model) => model.setData(
+        {
+          PhoneNumberValueKey: phoneNumberController.text,
+        },
+      );
+
+  /// Calls dispose on all the generated controllers and focus nodes
+  void disposeForm() {
+    // The dispose function for a TextEditingController sets all listeners to null
+
+    phoneNumberController.dispose();
+  }
+}
+
+extension ValueProperties on FormViewModel {
+  String get phoneNumberValue => this.formValueMap[PhoneNumberValueKey];
+}
