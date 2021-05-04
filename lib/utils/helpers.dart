71b084c9b487b5c1ca597b28181513a4a9143bf4
calebdeji <calebdeji06@gmail.com>
2021-05-04 10:38:59 +0100

    ✨ feature: add utility functions

--- lib/utils/helpers.dart
+++ lib/utils/helpers.dart
@@ -3,3 +3,11 @@ import 'package:flutter/rendering.dart';
 Color getColorFromHexValue(String hexValue) {
   return Color(int.parse('0xFF$hexValue'.toUpperCase()));
 }
+
+String appendNigeriaCallingCodeToPhoneNumber(String phoneNumber) {
+  const nigeriaPhoneNumberLength = 11;
+  if (phoneNumber.length == nigeriaPhoneNumberLength) {
+    return "+234${phoneNumber.substring(1)}";
+  }
+  return phoneNumber;
+}
